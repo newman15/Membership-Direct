136a7129edd14f508556aa6b9246ca2cf9e7a220
newman15 <47096479+newman15@users.noreply.github.com>
2020-04-29 15:03:12 -0700

    Basic Template Creation and Design

---
+++ functions.js
@@ -0,0 +1,10 @@
+/* 
+Author:     Aaron Newman
+Purpose:    Web Application JS Functions
+Rights:     All rights reserved to Membership Direct Inc. 
+Version:    2.0 
+*/
+
+function login(){
+    window.alert("Button has been clicked.");
+}
