8b2d7946dd76f2af785d47dc19e5327906a81b1d
newman15 <47096479+newman15@users.noreply.github.com>
2020-05-30 00:19:53 -0700

    update to claims-view styling

--- claims-view.php
+++ claims-view.php
@@ -105,7 +105,27 @@
 
             }
             else{
-                echo "You have no existing claims at this time";
+                echo <<<HTML
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <title>View Claims</title>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+                    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+                <style>
+                    a.nav-link:hover, a.nav-link:active{
+                        font-size: 150%;
+                    }
+                    h2{
+                        text-align: center;
+                    }
+                </style>
+                <h2>You have no existing claims at this time</h2>
+                HTML;
             }
 
             $dbh = null;
