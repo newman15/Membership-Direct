17443d29769d3517392dd4507140cb45c6e11105
newman15 <47096479+newman15@users.noreply.github.com>
2020-06-01 15:39:01 -0700

    More updates to sign-up-form error messages

--- functions.js
+++ functions.js
@@ -2,7 +2,7 @@
 Author:     Aaron Newman
 Purpose:    Web Application JS Functions
 Rights:     All rights reserved to Membership Direct Inc. 
-Version:    2.0 
+Version:    1.0 
 */
 
 // Test function
@@ -20,6 +20,26 @@ function termsAgreeIcon(){
     document.getElementById('terms-agreement').innerHTML='done_all';
 }
 
-// function checkTermsAgreement(){
+// Function to confirm both passwords are the same in sign-up-page
+function confirmPasswords(){
+    if(document.getElementById('pswd').value == document.getElementById('pswd-verify').value){
+        document.getElementById('message').style.color = 'green';
+        document.getElementById('message').innerHTML = 'Passwords Match';
+        document.getElementById('sign-up-btn').disabled = false;
+    }
+    else{
+        document.getElementById('message').style.color = 'red';
+        document.getElementById('message').innerHTML = 'Passwords do NOT match. Please retype password';
+        document.getElementById('sign-up-btn').disabled = true;
+    }
+}
 
-// }
+// jQuery for Terms and Conditions
+function termsAndConditions(){
+    $(document).ready(function(){
+        $("#sign-up-btn").hide();
+        $("#agree-btn").click(function(){
+          $("#sign-up-btn").show();
+        });
+    });
+}
